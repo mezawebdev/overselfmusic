51a0ab8d22476210a94912ee1cab72936b2f7de9
ameza <ameza@rescueagency.com>
2021-03-11 02:25:16 -0800

    added videos page

--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,5 +1,50 @@
+<template>
+    <div id="videos-page">
+        <div class="container">
+            <Title>VIDEOS</Title>
+            <div class="grid">
+                <iframe muted autoplay width="560" height="315" src="https://www.youtube.com/embed/lDIm4dA4VP4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/jvESyI2G6aw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Nf-s62B9ycg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Ki-ISdoNRIs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script>
-    export default {
-        middleware: "youtube-redirect"
-    }
+    import Title from "~/components/Layout/Title.vue";
+    export default { components: { Title } }
 </script>
+
+<style lang="scss">
+    #videos-page {
+        margin-top : 50px;
+        box-sizing : border-box;
+
+        * {
+            box-sizing : border-box;
+        }
+
+        .grid {
+            @media (min-width : 767px) {
+                display               : grid;
+                grid-template-columns : minmax(0, 1fr) minmax(0, 1fr);
+                grid-gap              : 50px;
+            }
+
+            iframe {
+                margin-bottom : 15px;
+                width         : 100%;
+                // width         : 755px;
+                // height        : 424px;
+                // max-width     : 100%;
+                // max-height    : calc((100vw - 40px) / (16/9));
+
+                @media (min-width : 767px) {
+                    margin-bottom : 0px;
+                }
+            }
+        }
+    }
+</style>
